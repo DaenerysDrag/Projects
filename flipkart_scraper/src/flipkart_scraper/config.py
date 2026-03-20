767ff6409953c140ad5583ff53c22b0044d091fe
DaenerysDrag <karan.makol1@gmail.com>
2026-03-21 02:42:01 +0530

    Delete flipkart_scraper directory

--- flipkart_scraper/src/flipkart_scraper/config.py
+++
@@ -1,9 +0,0 @@
-# List of brands to scrape
-BRANDS = ["Samsung", "LG", "Whirlpool", "Haier"]
-
-# Excel file settings
-EXCEL_FILENAME = 'Flipkart_WashingMachines.xlsx'
-
-# Scraper settings
-RETRIES = 3
-BACKOFF_FACTOR = 0.5
